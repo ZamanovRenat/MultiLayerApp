1fd89ff39aa9ac0d9e3abcd9e7c051291bba7c74
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-15 11:25:18 +0300

    Добавление интерфейса репозитория БД

---
+++ MultiLayerApp.DAL.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MultiLayerApp.DAL.Core.Domian.Entities.Base;
+
+namespace MultiLayerApp.DAL.Core.Interfaces
+{
+    public interface IRepository<T>
+        where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(Guid id);
+    }
+}
