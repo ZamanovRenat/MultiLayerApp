40e690e8e4a49ebbe9aaa6790a80f7006a91dafe
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-15 11:43:30 +0300

    Добавление класса контекста БД

---
+++ MultiLayerApp.DAL.DataAccess/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiLayerApp.DAL.Core.Domian.Entities;
+
+namespace MultiLayerApp.DAL.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DataContext(DbContextOptions<DataContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+            // Database.EnsureCreated();
+        }
+        //Конфигурирование моделей
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
