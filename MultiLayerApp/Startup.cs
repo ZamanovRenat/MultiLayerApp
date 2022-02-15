4fcaa2e73c56a15358ef4f16122c9839b38d6574
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-15 16:15:23 +0300

    Регистрация зависимостей и промежуточного ПО

--- MultiLayerApp/Startup.cs
+++ MultiLayerApp/Startup.cs
@@ -7,6 +7,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MultiLayerApp.DAL.Core.Domian.Entities;
+using MultiLayerApp.DAL.Core.Interfaces;
+using MultiLayerApp.DAL.DataAccess;
+using MultiLayerApp.DAL.DataAccess.Initial;
+using MultiLayerApp.DAL.DataAccess.Repositories;
+using MultiLayerApp.Models;
 
 namespace MultiLayerApp
 {
@@ -22,12 +29,18 @@ namespace MultiLayerApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=productsdb;Trusted_Connection=True;";
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+            services.AddScoped<IDbInitializer, DataDbInitializer>();
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
+            dbInitializer.InitializeDb();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
