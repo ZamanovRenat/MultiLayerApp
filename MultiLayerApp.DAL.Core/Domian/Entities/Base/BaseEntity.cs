b47f3c8e6e5736b9aeed958bac5aadfb95eaaaa9
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-15 11:19:57 +0300

    Добавление базовой сущности и сущности телефон

---
+++ MultiLayerApp.DAL.Core/Domian/Entities/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MultiLayerApp.DAL.Core.Domian.Entities.Base
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ MultiLayerApp.DAL.Core/Domian/Entities/Phone.cs
@@ -0,0 +1,11 @@
+﻿using MultiLayerApp.DAL.Core.Domian.Entities.Base;
+
+namespace MultiLayerApp.DAL.Core.Domian.Entities
+{
+    public class Phone : BaseEntity
+    {
+        public string Name { get; set; }    // название 
+        public string Company { get; set; } // производитель
+        public int Price { get; set; }  // цена
+    }
+}
