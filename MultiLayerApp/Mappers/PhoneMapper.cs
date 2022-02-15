f97b4085357949c1ed3b3add79d3a8746a3c5298
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-15 14:28:40 +0300

    Добавление класса маппера сущности телефон

---
+++ MultiLayerApp/Mappers/PhoneMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MultiLayerApp.DAL.Core.Domian.Entities;
+using MultiLayerApp.Models;
+
+namespace MultiLayerApp.Mappers
+{
+    public class PhoneMapper
+    {
+        public static Phone MapFromModel(PhoneViewModel model, Phone phone)
+        {
+            if (phone == null)
+            {
+                phone = new Phone();
+                phone.Id = Guid.NewGuid();
+            }
+
+            phone.Name = model.Name;
+            phone.Company = model.Company;
+            phone.Price = model.Price;
+
+            return phone;
+        }
+    }
+}
